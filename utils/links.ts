4db0da702bb6c88cc1a999d571784d4a00e617c6
Michael Bernard <104996295+Upxero@users.noreply.github.com>
2025-05-18 04:59:17 +0200

    Change all links to Dutch

--- utils/links.ts
+++ utils/links.ts
@@ -9,7 +9,7 @@ export const links: NavLink[] = [
   { href: '/bookings ', label: 'bookings' },
   { href: '/reviews ', label: 'reviews' },
   { href: '/reservaties ', label: 'reservaties' },
-  { href: '/bedrijf/toevoegen ', label: 'bedrijf toevoegen' },
+  { href: '/bedrijf/create ', label: 'bedrijf toevoegen' },
   { href: '/bedrijven', label: 'mijn bedrijven' },
   { href: '/admin', label: 'admin' },
   { href: '/profiel ', label: 'profiel' },
