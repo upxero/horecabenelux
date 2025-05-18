939b92cb5d7b2b5f60df41ff30b6ac76bfe848a0
Michael Bernard <104996295+Upxero@users.noreply.github.com>
2025-05-18 08:34:34 +0200

    Translate Profile to Dutch

--- app/profiel/page.tsx
+++ app/profiel/page.tsx
@@ -12,36 +12,36 @@ async function ProfilePage() {
 
   return (
     <section>
-      <h1 className='text-2xl font-semibold mb-8 capitalize'>user profile</h1>
+      <h1 className='text-2xl font-semibold mb-8 capitalize'>mijn profiel</h1>
       <div className='border p-8 rounded-md '>
         <ImageInputContainer
           image={profile.profileImage}
           name={profile.username}
           action={updateProfileImageAction}
-          text='Update Profile Image'
+          text='Profielfoto updaten'
         />
         <FormContainer action={updateProfileAction}>
           <div className='grid md:grid-cols-2 gap-4 mt-4'>
             <FormInput
               type='text'
               name='firstName'
-              label='First Name'
+              label='Voornaam'
               defaultValue={profile.firstName}
             />
             <FormInput
               type='text'
               name='lastName'
-              label='Last Name'
+              label='Achternaam'
               defaultValue={profile.lastName}
             />
             <FormInput
               type='text'
               name='username'
-              label='Username'
+              label='Gebruikersnaam'
               defaultValue={profile.username}
             />
           </div>
-          <SubmitButton text='update profile' className='mt-8' />
+          <SubmitButton text='update profiel' className='mt-8' />
         </FormContainer>
       </div>
     </section>
