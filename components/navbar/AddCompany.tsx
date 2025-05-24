f35b6a02ae2500394a01f7b4674ec5877fa86b5f
Michael Bernard <104996295+Upxero@users.noreply.github.com>
2025-05-25 01:22:01 +0200

    add auth in AddCompany

--- components/navbar/AddCompany.tsx
+++ components/navbar/AddCompany.tsx
@@ -2,11 +2,25 @@
 
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
+import { SignedIn, SignedOut, SignInButton, SignUpButton } from '@clerk/nextjs';
 
 export default function AddCompany() {
   return (
-    <Link href='/bedrijven/create'>
-      <Button variant='outline'>Bedrijf toevoegen</Button>
-    </Link>
+    <>
+      {/* Alleen zichtbaar als gebruiker is ingelogd */}
+      <SignedIn>
+        <Link href='/bedrijven/create'>
+          <Button variant='outline'>Bedrijf toevoegen</Button>
+        </Link>
+      </SignedIn>
+
+      {/* Als gebruiker NIET is ingelogd, toon login modal knop */}
+      <SignedOut>
+        <SignInButton mode='modal'>
+          <Button variant='outline'>Bedrijf toevoegen</Button>
+        </SignInButton>
+      </SignedOut>
+    </>
   );
 }
+
