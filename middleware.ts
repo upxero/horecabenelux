6db2f2ed14d07fdf5d3f15f2074c4ff2fd853dc9
Michael Bernard <104996295+Upxero@users.noreply.github.com>
2025-05-31 05:55:33 +0200

    add bedrijfsgegevens in isPublicRoute

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
-const isPublicRoute = createRouteMatcher(['/', '/properties(.*)']);
+const isPublicRoute = createRouteMatcher(['/', '/properties(.*)', '/bedrijfsgegevens']);
 const isAdminRoute = createRouteMatcher(['/admin(.*)']);
 
 export default clerkMiddleware((auth, req) => {
