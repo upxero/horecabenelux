b20753d68262a6f20922ce918f68af73c155851d
Michael Bernard <104996295+Upxero@users.noreply.github.com>
2025-06-01 05:19:47 +0200

    add contact in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
-const isPublicRoute = createRouteMatcher(['/', '/properties(.*)', '/bedrijfsgegevens']);
+const isPublicRoute = createRouteMatcher(['/', '/properties(.*)', '/bedrijfsgegevens', '/contact']);
 const isAdminRoute = createRouteMatcher(['/admin(.*)']);
 
 export default clerkMiddleware((auth, req) => {
