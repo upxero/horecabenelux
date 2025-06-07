647e13b0ece92e2c91af89ef3d343b7093ec022b
Michael Bernard <104996295+Upxero@users.noreply.github.com>
2025-06-07 05:16:01 +0200

    add SuccessMessage to Home

--- app/page.tsx
+++ app/page.tsx
@@ -3,31 +3,43 @@ import CategoriesList from '@/components/home/CategoriesList';
 import PropertiesContainer from '@/components/home/PropertiesContainer';
 import Hero from '@/components/home/Hero';
 import Banner from '@/components/home/Banner';
+import SuccessMessage from '@/components/home/SuccessMessage';
 import { Suspense } from 'react';
+
 function HomePage({
   searchParams,
 }: {
-  searchParams: { category?: string; search?: string };
+  searchParams: {
+    category?: string;
+    search?: string;
+    success?: string;
+  };
 }) {
   return (
     <section>
       <Hero />
+      {/* ✅ Succesmelding tonen indien van toepassing */}
+      <SuccessMessage success={searchParams.success} />
+
       <section id="categories-list">
         <CategoriesList
           category={searchParams.category}
           search={searchParams.search}
         />
       </section>
+
       <Suspense fallback={<LoadingCards />}>
         <PropertiesContainer
           category={searchParams.category}
           search={searchParams.search}
         />
       </Suspense>
-      <section className="mt-20"> 
-      <Banner /> 
+
+      <section className="mt-20">
+        <Banner />
       </section>
     </section>
   );
 }
+
 export default HomePage;
