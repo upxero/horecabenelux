0c18e909cd604f6107ec128a416b98ce9c566fa4
Michael Bernard <104996295+Upxero@users.noreply.github.com>
2025-06-08 06:35:35 +0200

    required message image

--- components/form/ImageInput.tsx
+++ components/form/ImageInput.tsx
@@ -1,8 +1,19 @@
+import { useState } from 'react';
 import { Input } from '../ui/input';
 import { Label } from '../ui/label';
 
 function ImageInput() {
   const name = 'image';
+  const [error, setError] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files || e.target.files.length === 0) {
+      setError('Voeg een afbeelding toe');
+    } else {
+      setError('');
+    }
+  };
+
   return (
     <div className='mb-2'>
       <Label htmlFor={name} className='capitalize'>
@@ -12,11 +23,14 @@ function ImageInput() {
         id={name}
         name={name}
         type='file'
-        required
         accept='image/*'
         className='max-w-xs'
+        onChange={handleChange}
+        required // mag blijven voor desktop
       />
+      {error && <p className="text-red-600 mt-1">{error}</p>}
     </div>
   );
 }
+
 export default ImageInput;
